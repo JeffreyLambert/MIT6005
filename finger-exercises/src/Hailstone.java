3178a516a9759c6dc27a80f956e9d256544fe0df
Jeff <jeffreyrlambert@gmail.com>
2021-10-03 09:42:04 -0600

    JRL: Created class to do hailstone sequence generation

---
+++ finger-exercises/src/Hailstone.java
@@ -0,0 +1,45 @@
+public class Hailstone {
+    Integer n;
+
+    // Initialize Class
+    public Hailstone(Integer n) {
+        this.n = n;
+    }
+
+    // Setter method for attribute n
+    public void setN(Integer value) {
+        this.n = value;
+    }
+
+    // Getter method for attribute n
+    public Integer getN() {
+        return this.n;
+    }
+
+    // Create method for generating next sequence
+    public void nextElement() {
+        if (this.n % 2 == 0)
+            this.setN(this.n / 2);
+        else
+            this.setN(3*this.n + 1);
+    }
+
+    // Create method for looping through sequence
+    public void resolveSequence() {
+        while (this.n != 1) {
+            this.nextElement();
+            System.out.println(this.n);
+        }
+
+        System.out.println("Sequence resolved!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running Hailstone Sequence!");
+
+        // Initialize Hailstone object
+        Hailstone hs = new Hailstone(7);
+        System.out.println(hs.getN());
+        hs.resolveSequence();
+    }
+}
